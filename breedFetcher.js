721c14a67fbc35dfaf4817e30fe8fc94ce8b4d2f
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-07 20:37:49 +0000

    Access first entry in the data array

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,15 @@
 const request = require('request');
 
 const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const breed = process.argv[2];
+const breedName = process.argv[2];
 
-const breedFetcher = (breedName , cb) => {
+
   request(url + breedName, (error, response, body) => {
     if (error || response.statusCode !== 200) {
       console.log('Doh! Something is wrong with the URL.\n', error)
-      return cb(error, null);
+      return;
     }
-
     const data = JSON.parse(body);
-    let desc = data[0].description
-    console.log(typeof body)
+    console.log(data[0]);
   })
-}
+
