30800775127b781d913cc3e6c45f1e84c2126191
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-07 20:58:28 +0000

    Added fetchBreedDescription function and moved request into it

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 const breedName = process.argv[2];
 
-
+const fetchBreedDescription = function(breedName, cb) {
   request(url + breedName, (error, response, body) => {
     if (error || response.statusCode !== 200) {
       console.log('Doh! Something is wrong with the URL.\n', error)
@@ -17,4 +17,6 @@ const breedName = process.argv[2];
       console.log('Doh! The requested breed was not found!')
     }
   })
+};
+
 
